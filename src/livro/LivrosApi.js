e8a9163aa0f4a0bffe2618496c945b70515dcac6
Carlos Eduardo <142689457+carlosfernandino@users.noreply.github.com>
2024-11-12 20:14:37 +0000

    Atualização de URL e variavel URL

--- src/livro/LivrosApi.js
+++ src/livro/LivrosApi.js
@@ -1,19 +1,19 @@
 const URL =
+  "https://api-books-dot-api-samples-423102.uc.r.appspot.com/api-docs";
 
 export async function findAll() {
-    const requestInit = {
-      method: "GET",
-      Headers: {
-        Authorization: "Bearer 12120117",
-      },
-    };
-  
-    const responseHttp = await fetch("URL", requestInit);
-  
-    if (responseHttp.ok) {
-      return await responseHttp.json();
-    } else {
-      throw new Error("Falha ao consultar os livros");
-    }
+  const requestInit = {
+    method: "GET",
+    Headers: {
+      Authorization: "Bearer 12120117",
+    },
+  };
+
+  const responseHttp = await fetch(URL, requestInit);
+
+  if (responseHttp.ok) {
+    return await responseHttp.json();
+  } else {
+    throw new Error("Falha ao consultar os livros");
   }
-  
+}
